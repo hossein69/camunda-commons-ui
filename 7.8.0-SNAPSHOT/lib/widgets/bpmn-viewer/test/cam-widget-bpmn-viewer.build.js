bd8d70ab046bd78bc0fb2aff8ba825dd33d9f470
camunda-jenkins <ci@camunda.com>
2017-11-17 10:13:23 +0100

    "gh-pages update"

--- 7.8.0-SNAPSHOT/lib/widgets/bpmn-viewer/test/cam-widget-bpmn-viewer.build.js
+++ 7.8.0-SNAPSHOT/lib/widgets/bpmn-viewer/test/cam-widget-bpmn-viewer.build.js
@@ -92100,16 +92100,244 @@ arguments[4][71][0].apply(exports,arguments)
 },{"./lib/moddle":849,"dup":71}],846:[function(require,module,exports){
 arguments[4][72][0].apply(exports,arguments)
 },{"dup":72}],847:[function(require,module,exports){
-arguments[4][73][0].apply(exports,arguments)
-},{"./ns":850,"dup":73,"lodash/collection/forEach":870,"lodash/object/assign":983,"lodash/object/pick":988}],848:[function(require,module,exports){
+'use strict';
+
+var pick = require('lodash/object/pick'),
+    assign = require('lodash/object/assign'),
+    forEach = require('lodash/collection/forEach');
+
+var parseNameNs = require('./ns').parseName;
+
+
+function DescriptorBuilder(nameNs) {
+  this.ns = nameNs;
+  this.name = nameNs.name;
+  this.allTypes = [];
+  this.allTypesByName = {};
+  this.properties = [];
+  this.propertiesByName = {};
+}
+
+module.exports = DescriptorBuilder;
+
+
+DescriptorBuilder.prototype.build = function() {
+  return pick(this, [
+    'ns',
+    'name',
+    'allTypes',
+    'allTypesByName',
+    'properties',
+    'propertiesByName',
+    'bodyProperty',
+    'idProperty'
+  ]);
+};
+
+/**
+ * Add property at given index.
+ *
+ * @param {Object} p
+ * @param {Number} [idx]
+ * @param {Boolean} [validate=true]
+ */
+DescriptorBuilder.prototype.addProperty = function(p, idx, validate) {
+
+  if (typeof idx === 'boolean') {
+    validate = idx;
+    idx = undefined;
+  }
+
+  this.addNamedProperty(p, validate !== false);
+
+  var properties = this.properties;
+
+  if (idx !== undefined) {
+    properties.splice(idx, 0, p);
+  } else {
+    properties.push(p);
+  }
+};
+
+
+DescriptorBuilder.prototype.replaceProperty = function(oldProperty, newProperty, replace) {
+  var oldNameNs = oldProperty.ns;
+
+  var props = this.properties,
+      propertiesByName = this.propertiesByName,
+      rename = oldProperty.name !== newProperty.name;
+
+  if (oldProperty.isId) {
+    if (!newProperty.isId) {
+      throw new Error(
+        'property <' + newProperty.ns.name + '> must be id property ' +
+        'to refine <' + oldProperty.ns.name + '>');
+    }
+
+    this.setIdProperty(newProperty, false);
+  }
+
+  if (oldProperty.isBody) {
+
+    if (!newProperty.isBody) {
+      throw new Error(
+        'property <' + newProperty.ns.name + '> must be body property ' +
+        'to refine <' + oldProperty.ns.name + '>');
+    }
+
+    // TODO: Check compatibility
+    this.setBodyProperty(newProperty, false);
+  }
+
+  // validate existence and get location of old property
+  var idx = props.indexOf(oldProperty);
+  if (idx === -1) {
+    throw new Error('property <' + oldNameNs.name + '> not found in property list');
+  }
+
+  // remove old property
+  props.splice(idx, 1);
+
+  // replacing the named property is intentional
+  //
+  //  * validate only if this is a "rename" operation
+  //  * add at specific index unless we "replace"
+  //
+  this.addProperty(newProperty, replace ? undefined : idx, rename);
+
+  // make new property available under old name
+  propertiesByName[oldNameNs.name] = propertiesByName[oldNameNs.localName] = newProperty;
+};
+
+
+DescriptorBuilder.prototype.redefineProperty = function(p, targetPropertyName, replace) {
+
+  var nsPrefix = p.ns.prefix;
+  var parts = targetPropertyName.split('#');
+
+  var name = parseNameNs(parts[0], nsPrefix);
+  var attrName = parseNameNs(parts[1], name.prefix).name;
+
+  var redefinedProperty = this.propertiesByName[attrName];
+  if (!redefinedProperty) {
+    throw new Error('refined property <' + attrName + '> not found');
+  } else {
+    this.replaceProperty(redefinedProperty, p, replace);
+  }
+
+  delete p.redefines;
+};
+
+DescriptorBuilder.prototype.addNamedProperty = function(p, validate) {
+  var ns = p.ns,
+      propsByName = this.propertiesByName;
+
+  if (validate) {
+    this.assertNotDefined(p, ns.name);
+    this.assertNotDefined(p, ns.localName);
+  }
+
+  propsByName[ns.name] = propsByName[ns.localName] = p;
+};
+
+DescriptorBuilder.prototype.removeNamedProperty = function(p) {
+  var ns = p.ns,
+      propsByName = this.propertiesByName;
+
+  delete propsByName[ns.name];
+  delete propsByName[ns.localName];
+};
+
+DescriptorBuilder.prototype.setBodyProperty = function(p, validate) {
+
+  if (validate && this.bodyProperty) {
+    throw new Error(
+      'body property defined multiple times ' +
+      '(<' + this.bodyProperty.ns.name + '>, <' + p.ns.name + '>)');
+  }
+
+  this.bodyProperty = p;
+};
+
+DescriptorBuilder.prototype.setIdProperty = function(p, validate) {
+
+  if (validate && this.idProperty) {
+    throw new Error(
+      'id property defined multiple times ' +
+      '(<' + this.idProperty.ns.name + '>, <' + p.ns.name + '>)');
+  }
+
+  this.idProperty = p;
+};
+
+DescriptorBuilder.prototype.assertNotDefined = function(p, name) {
+  var propertyName = p.name,
+      definedProperty = this.propertiesByName[propertyName];
+
+  if (definedProperty) {
+    throw new Error(
+      'property <' + propertyName + '> already defined; ' +
+      'override of <' + definedProperty.definedBy.ns.name + '#' + definedProperty.ns.name + '> by ' +
+      '<' + p.definedBy.ns.name + '#' + p.ns.name + '> not allowed without redefines');
+  }
+};
+
+DescriptorBuilder.prototype.hasProperty = function(name) {
+  return this.propertiesByName[name];
+};
+
+DescriptorBuilder.prototype.addTrait = function(t, inherited) {
+
+  var typesByName = this.allTypesByName,
+      types = this.allTypes;
+
+  var typeName = t.name;
+
+  if (typeName in typesByName) {
+    return;
+  }
+
+  forEach(t.properties, function(p) {
+
+    // clone property to allow extensions
+    p = assign({}, p, {
+      name: p.ns.localName,
+      inherited: inherited
+    });
+
+    Object.defineProperty(p, 'definedBy', {
+      value: t
+    });
+
+    var replaces = p.replaces,
+        redefines = p.redefines;
+
+    // add replace/redefine support
+    if (replaces || redefines) {
+      this.redefineProperty(p, replaces || redefines, replaces);
+    } else {
+      if (p.isBody) {
+        this.setBodyProperty(p);
+      }
+      if (p.isId) {
+        this.setIdProperty(p);
+      }
+      this.addProperty(p);
+    }
+  }, this);
+
+  types.push(t);
+  typesByName[typeName] = t;
+};
+
+},{"./ns":850,"lodash/collection/forEach":870,"lodash/object/assign":983,"lodash/object/pick":988}],848:[function(require,module,exports){
 arguments[4][74][0].apply(exports,arguments)
 },{"./base":846,"dup":74,"lodash/collection/forEach":870}],849:[function(require,module,exports){
 'use strict';
 
 var isString = require('lodash/lang/isString'),
     isObject = require('lodash/lang/isObject'),
-    forEach = require('lodash/collection/forEach'),
-    find = require('lodash/collection/find');
+    forEach = require('lodash/collection/forEach');
 
 
 var Factory = require('./factory'),
@@ -92242,7 +92470,10 @@ Moddle.prototype.createAny = function(name, nsUri, properties) {
   var nameNs = parseNameNs(name);
 
   var element = {
-    $type: name
+    $type: name,
+    $instanceOf: function(type) {
+      return type === this.$type;
+    }
   };
 
   var descriptor = {
@@ -92309,9 +92540,7 @@ Moddle.prototype.hasType = function(element, type) {
 
   var descriptor = element.$model.getElementDescriptor(element);
 
-  return !!find(descriptor.allTypes, function(t) {
-    return t.name === type;
-  });
+  return (type in descriptor.allTypesByName);
 };
 
 /**
@@ -92328,7 +92557,7 @@ Moddle.prototype.getTypeDescriptor = function(type) {
   return this.registry.typeMap[type];
 };
 
-},{"./factory":848,"./ns":850,"./properties":851,"./registry":852,"lodash/collection/find":869,"lodash/collection/forEach":870,"lodash/lang/isObject":980,"lodash/lang/isString":981}],850:[function(require,module,exports){
+},{"./factory":848,"./ns":850,"./properties":851,"./registry":852,"lodash/collection/forEach":870,"lodash/lang/isObject":980,"lodash/lang/isString":981}],850:[function(require,module,exports){
 arguments[4][76][0].apply(exports,arguments)
 },{"dup":76}],851:[function(require,module,exports){
 arguments[4][77][0].apply(exports,arguments)
@@ -92373,12 +92602,17 @@ Registry.prototype.registerPackage = function(pkg) {
   // copy package
   pkg = assign({}, pkg);
 
+  var pkgMap = this.packageMap;
+
+  ensureAvailable(pkgMap, pkg, 'prefix');
+  ensureAvailable(pkgMap, pkg, 'uri');
+
   // register types
   forEach(pkg.types, function(descriptor) {
     this.registerType(descriptor, pkg);
   }, this);
 
-  this.packageMap[pkg.uri] = this.packageMap[pkg.prefix] = pkg;
+  pkgMap[pkg.uri] = pkgMap[pkg.prefix] = pkg;
   this.packages.push(pkg);
 };
 
@@ -92518,6 +92752,19 @@ Registry.prototype.getEffectiveDescriptor = function(name) {
 Registry.prototype.definePackage = function(target, pkg) {
   this.properties.define(target, '$pkg', { value: pkg });
 };
+
+
+
+///////// helpers ////////////////////////////
+
+function ensureAvailable(packageMap, pkg, identifierKey) {
+
+  var value = pkg[identifierKey];
+
+  if (value in packageMap) {
+    throw new Error('package with ' + identifierKey + ' <' + value + '> already defined');
+  }
+}
 
 },{"./descriptor-builder":847,"./ns":850,"./types":853,"lodash/collection/forEach":870,"lodash/object/assign":983}],853:[function(require,module,exports){
 arguments[4][79][0].apply(exports,arguments)
--- 7.8.0-SNAPSHOT/lib/widgets/inline-field/test/cam-widget-inline-field.build.js
+++ 7.8.0-SNAPSHOT/lib/widgets/inline-field/test/cam-widget-inline-field.build.js
@@ -92029,16 +92029,244 @@ arguments[4][70][0].apply(exports,arguments)
 },{"./lib/moddle":848,"dup":70}],845:[function(require,module,exports){
 arguments[4][71][0].apply(exports,arguments)
 },{"dup":71}],846:[function(require,module,exports){
-arguments[4][72][0].apply(exports,arguments)
-},{"./ns":849,"dup":72,"lodash/collection/forEach":869,"lodash/object/assign":982,"lodash/object/pick":987}],847:[function(require,module,exports){
+'use strict';
+
+var pick = require('lodash/object/pick'),
+    assign = require('lodash/object/assign'),
+    forEach = require('lodash/collection/forEach');
+
+var parseNameNs = require('./ns').parseName;
+
+
+function DescriptorBuilder(nameNs) {
+  this.ns = nameNs;
+  this.name = nameNs.name;
+  this.allTypes = [];
+  this.allTypesByName = {};
+  this.properties = [];
+  this.propertiesByName = {};
+}
+
+module.exports = DescriptorBuilder;
+
+
+DescriptorBuilder.prototype.build = function() {
+  return pick(this, [
+    'ns',
+    'name',
+    'allTypes',
+    'allTypesByName',
+    'properties',
+    'propertiesByName',
+    'bodyProperty',
+    'idProperty'
+  ]);
+};
+
+/**
+ * Add property at given index.
+ *
+ * @param {Object} p
+ * @param {Number} [idx]
+ * @param {Boolean} [validate=true]
+ */
+DescriptorBuilder.prototype.addProperty = function(p, idx, validate) {
+
+  if (typeof idx === 'boolean') {
+    validate = idx;
+    idx = undefined;
+  }
+
+  this.addNamedProperty(p, validate !== false);
+
+  var properties = this.properties;
+
+  if (idx !== undefined) {
+    properties.splice(idx, 0, p);
+  } else {
+    properties.push(p);
+  }
+};
+
+
+DescriptorBuilder.prototype.replaceProperty = function(oldProperty, newProperty, replace) {
+  var oldNameNs = oldProperty.ns;
+
+  var props = this.properties,
+      propertiesByName = this.propertiesByName,
+      rename = oldProperty.name !== newProperty.name;
+
+  if (oldProperty.isId) {
+    if (!newProperty.isId) {
+      throw new Error(
+        'property <' + newProperty.ns.name + '> must be id property ' +
+        'to refine <' + oldProperty.ns.name + '>');
+    }
+
+    this.setIdProperty(newProperty, false);
+  }
+
+  if (oldProperty.isBody) {
+
+    if (!newProperty.isBody) {
+      throw new Error(
+        'property <' + newProperty.ns.name + '> must be body property ' +
+        'to refine <' + oldProperty.ns.name + '>');
+    }
+
+    // TODO: Check compatibility
+    this.setBodyProperty(newProperty, false);
+  }
+
+  // validate existence and get location of old property
+  var idx = props.indexOf(oldProperty);
+  if (idx === -1) {
+    throw new Error('property <' + oldNameNs.name + '> not found in property list');
+  }
+
+  // remove old property
+  props.splice(idx, 1);
+
+  // replacing the named property is intentional
+  //
+  //  * validate only if this is a "rename" operation
+  //  * add at specific index unless we "replace"
+  //
+  this.addProperty(newProperty, replace ? undefined : idx, rename);
+
+  // make new property available under old name
+  propertiesByName[oldNameNs.name] = propertiesByName[oldNameNs.localName] = newProperty;
+};
+
+
+DescriptorBuilder.prototype.redefineProperty = function(p, targetPropertyName, replace) {
+
+  var nsPrefix = p.ns.prefix;
+  var parts = targetPropertyName.split('#');
+
+  var name = parseNameNs(parts[0], nsPrefix);
+  var attrName = parseNameNs(parts[1], name.prefix).name;
+
+  var redefinedProperty = this.propertiesByName[attrName];
+  if (!redefinedProperty) {
+    throw new Error('refined property <' + attrName + '> not found');
+  } else {
+    this.replaceProperty(redefinedProperty, p, replace);
+  }
+
+  delete p.redefines;
+};
+
+DescriptorBuilder.prototype.addNamedProperty = function(p, validate) {
+  var ns = p.ns,
+      propsByName = this.propertiesByName;
+
+  if (validate) {
+    this.assertNotDefined(p, ns.name);
+    this.assertNotDefined(p, ns.localName);
+  }
+
+  propsByName[ns.name] = propsByName[ns.localName] = p;
+};
+
+DescriptorBuilder.prototype.removeNamedProperty = function(p) {
+  var ns = p.ns,
+      propsByName = this.propertiesByName;
+
+  delete propsByName[ns.name];
+  delete propsByName[ns.localName];
+};
+
+DescriptorBuilder.prototype.setBodyProperty = function(p, validate) {
+
+  if (validate && this.bodyProperty) {
+    throw new Error(
+      'body property defined multiple times ' +
+      '(<' + this.bodyProperty.ns.name + '>, <' + p.ns.name + '>)');
+  }
+
+  this.bodyProperty = p;
+};
+
+DescriptorBuilder.prototype.setIdProperty = function(p, validate) {
+
+  if (validate && this.idProperty) {
+    throw new Error(
+      'id property defined multiple times ' +
+      '(<' + this.idProperty.ns.name + '>, <' + p.ns.name + '>)');
+  }
+
+  this.idProperty = p;
+};
+
+DescriptorBuilder.prototype.assertNotDefined = function(p, name) {
+  var propertyName = p.name,
+      definedProperty = this.propertiesByName[propertyName];
+
+  if (definedProperty) {
+    throw new Error(
+      'property <' + propertyName + '> already defined; ' +
+      'override of <' + definedProperty.definedBy.ns.name + '#' + definedProperty.ns.name + '> by ' +
+      '<' + p.definedBy.ns.name + '#' + p.ns.name + '> not allowed without redefines');
+  }
+};
+
+DescriptorBuilder.prototype.hasProperty = function(name) {
+  return this.propertiesByName[name];
+};
+
+DescriptorBuilder.prototype.addTrait = function(t, inherited) {
+
+  var typesByName = this.allTypesByName,
+      types = this.allTypes;
+
+  var typeName = t.name;
+
+  if (typeName in typesByName) {
+    return;
+  }
+
+  forEach(t.properties, function(p) {
+
+    // clone property to allow extensions
+    p = assign({}, p, {
+      name: p.ns.localName,
+      inherited: inherited
+    });
+
+    Object.defineProperty(p, 'definedBy', {
+      value: t
+    });
+
+    var replaces = p.replaces,
+        redefines = p.redefines;
+
+    // add replace/redefine support
+    if (replaces || redefines) {
+      this.redefineProperty(p, replaces || redefines, replaces);
+    } else {
+      if (p.isBody) {
+        this.setBodyProperty(p);
+      }
+      if (p.isId) {
+        this.setIdProperty(p);
+      }
+      this.addProperty(p);
+    }
+  }, this);
+
+  types.push(t);
+  typesByName[typeName] = t;
+};
+
+},{"./ns":849,"lodash/collection/forEach":869,"lodash/object/assign":982,"lodash/object/pick":987}],847:[function(require,module,exports){
 arguments[4][73][0].apply(exports,arguments)
 },{"./base":845,"dup":73,"lodash/collection/forEach":869}],848:[function(require,module,exports){
 'use strict';
 
 var isString = require('lodash/lang/isString'),
     isObject = require('lodash/lang/isObject'),
-    forEach = require('lodash/collection/forEach'),
-    find = require('lodash/collection/find');
+    forEach = require('lodash/collection/forEach');
 
 
 var Factory = require('./factory'),
@@ -92171,7 +92399,10 @@ Moddle.prototype.createAny = function(name, nsUri, properties) {
   var nameNs = parseNameNs(name);
 
   var element = {
-    $type: name
+    $type: name,
+    $instanceOf: function(type) {
+      return type === this.$type;
+    }
   };
 
   var descriptor = {
@@ -92238,9 +92469,7 @@ Moddle.prototype.hasType = function(element, type) {
 
   var descriptor = element.$model.getElementDescriptor(element);
 
-  return !!find(descriptor.allTypes, function(t) {
-    return t.name === type;
-  });
+  return (type in descriptor.allTypesByName);
 };
 
 /**
@@ -92257,7 +92486,7 @@ Moddle.prototype.getTypeDescriptor = function(type) {
   return this.registry.typeMap[type];
 };
 
-},{"./factory":847,"./ns":849,"./properties":850,"./registry":851,"lodash/collection/find":868,"lodash/collection/forEach":869,"lodash/lang/isObject":979,"lodash/lang/isString":980}],849:[function(require,module,exports){
+},{"./factory":847,"./ns":849,"./properties":850,"./registry":851,"lodash/collection/forEach":869,"lodash/lang/isObject":979,"lodash/lang/isString":980}],849:[function(require,module,exports){
 arguments[4][75][0].apply(exports,arguments)
 },{"dup":75}],850:[function(require,module,exports){
 arguments[4][76][0].apply(exports,arguments)
@@ -92302,12 +92531,17 @@ Registry.prototype.registerPackage = function(pkg) {
   // copy package
   pkg = assign({}, pkg);
 
+  var pkgMap = this.packageMap;
+
+  ensureAvailable(pkgMap, pkg, 'prefix');
+  ensureAvailable(pkgMap, pkg, 'uri');
+
   // register types
   forEach(pkg.types, function(descriptor) {
     this.registerType(descriptor, pkg);
   }, this);
 
-  this.packageMap[pkg.uri] = this.packageMap[pkg.prefix] = pkg;
+  pkgMap[pkg.uri] = pkgMap[pkg.prefix] = pkg;
   this.packages.push(pkg);
 };
 
@@ -92447,6 +92681,19 @@ Registry.prototype.getEffectiveDescriptor = function(name) {
 Registry.prototype.definePackage = function(target, pkg) {
   this.properties.define(target, '$pkg', { value: pkg });
 };
+
+
+
+///////// helpers ////////////////////////////
+
+function ensureAvailable(packageMap, pkg, identifierKey) {
+
+  var value = pkg[identifierKey];
+
+  if (value in packageMap) {
+    throw new Error('package with ' + identifierKey + ' <' + value + '> already defined');
+  }
+}
 
 },{"./descriptor-builder":846,"./ns":849,"./types":852,"lodash/collection/forEach":869,"lodash/object/assign":982}],852:[function(require,module,exports){
 arguments[4][78][0].apply(exports,arguments)
